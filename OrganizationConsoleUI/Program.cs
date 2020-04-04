e7d622f7ef1e0c4ae047c9cb49e8cc8dd5f96158
fausio luis <fausioluis@live.com>
2020-04-04 23:24:45 +0200

    adding ConsoleUI

---
+++ OrganizationConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OrgStanderdLibrary;
+
+namespace OrganizationConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee employee = new Employee()
+            {
+                EmployeeId = 1,
+                Name = "Fausio",
+                SalaryPerDay = 300,
+                HRA = 6.2
+            };
+
+            Console.WriteLine("The gross salary of {0} is {1}", employee.Name, employee.GetSalary(5));
+
+            Console.ReadKey();
+        }
+    }
+}
